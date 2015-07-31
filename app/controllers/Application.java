1bf6a18147713b98519ac1eae641b13f43528b3b
Jan Grape <jan.grape@crisp.se>
2015-07-31 14:54:20 +0200

    Divided into two views - with and without graph

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,20 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.empty;
+import views.html.graph;
 
 public class Application extends Controller {
 
-    public Result index() {
-        return ok(index.render());
-    }
+  public Result show(UrlParameter spreadsheetUrl) {
+    if (spreadsheetUrl != null)
+      return ok(graph.render(spreadsheetUrl.getValue().toString()));
+    else
+      return ok(empty.render());
+  }
 
+  public Result submit() {
+      return redirect(routes.Application.show(null));
+  }
 }
